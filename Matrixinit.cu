3f3f5447f1a1744b882c3ec2fee3148375b92b40
Vivekyadavgithub <59399015+Vivekyadavgithub@users.noreply.github.com>
2021-02-09 17:19:16 +0530

    Create Matrixinit.cu

---
+++ Matrixinit.cu
@@ -0,0 +1,36 @@
+%%cu
+#include <stdio.h>
+#include <cuda.h>
+#define N 5
+#define M 6
+__global__ void dkernal(unsigned int* matrix){
+    /*if(threadIdx.x == 0 &&blockIdx.x == 0 &&
+       threadIdx.y == 0 && blockIdx.y == 0 &&
+       threadIdx.z == 0 && blockIdx.z == 0){
+           printf("%d %d %d %d %d %d\n", gridDim.x, gridDim.y, gridDim.z, 
+                                         blockDim.x, blockDim.y, blockDim.z);
+       }*/
+       int i = threadIdx.x; 
+               matrix[threadIdx.y + (i * M)] = 
+                                   threadIdx.y + (i * M) ;
+       
+}
+
+int main(){
+    dim3 block(N, M, 1);
+    unsigned* matrix, *hmatrix;
+    cudaMalloc(&matrix, N * M * sizeof(unsigned));
+    hmatrix = (unsigned*)malloc(N * M * sizeof(unsigned));
+    dkernal <<<1, block>>>(matrix);
+    cudaMemcpy(hmatrix, matrix, N * M * sizeof(unsigned), 
+                                    cudaMemcpyDeviceToHost);
+    for(unsigned i = 0 ; i < N ; ++i)
+    {
+        for(unsigned j = 0 ; j < M ; ++j)
+        {
+            printf("%3d", hmatrix[i * M + j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
